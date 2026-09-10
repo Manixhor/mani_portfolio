6e71c7a77ae5d3bbe0558e22ea26a76451520e74
Manixhor <manigururam@hotmail.com>
2026-09-10 18:41:04 +0530

    Refresh cached frontend assets

--- portfolio/frontend/sw.js
+++ portfolio/frontend/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "mani-portfolio-v3";
+const CACHE_NAME = "mani-portfolio-v4";
 const APP_SHELL = [
   "/",
-  "/static/css/style.css?v=3",
-  "/static/js/main.js?v=3",
+  "/static/css/style.css?v=7",
+  "/static/js/main.js?v=4",
   "/manifest.webmanifest",
   "/static/icons/icon.svg"
 ];
